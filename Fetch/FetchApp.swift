5f1ef065941af779a61d3638d6a24cad621f2ea2
liuyi65 <yliu511@jh.edu>
2023-11-06 13:49:35 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Liu Yi on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
